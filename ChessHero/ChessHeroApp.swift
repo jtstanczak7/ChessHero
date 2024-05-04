234b520e60fa2e804c24990f8a0f193990665cd3
Jonathan Stanczak <jtstanczak7@verizon.net>
2024-05-03 23:26:20 -0400

    Initial Commit

---
+++ ChessHero/ChessHeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessHeroApp.swift
+//  ChessHero
+//
+//  Created by Jonathan Stanczak on 5/3/24.
+//
+
+import SwiftUI
+
+@main
+struct ChessHeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChessHero/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChessHero
+//
+//  Created by Jonathan Stanczak on 5/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
